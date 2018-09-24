28a0ad4a6ebb886e05665c4aaa876d7dad4476d0
Mumin Quadri <mumin.quadri25@gmail.com>
2018-09-25 01:15:23 +0530

    Adds initial project files.

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController:Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {         
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(t => t.Id == id));
+        }
+    }
+}
