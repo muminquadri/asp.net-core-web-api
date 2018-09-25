0c7e6e6d615c5b4c2b0c9a76ed631e0affe575b8
Mumin Quadri <mumin.quadri25@gmail.com>
2018-09-25 08:09:00 +0530

    Allows requests to be consumed in xml apart from default json(formatters and content negotiation)

--- CityInfo.Api/Startup.cs
+++ CityInfo.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -16,8 +17,10 @@ namespace CityInfo.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-                //logic to set json output property name to same as defined in class name. Normally we get id in JSON 
+            services.AddMvc().
+                AddMvcOptions(o=>o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));//this allows the
+            //request to be consumed in xml apart from default json.
+                //below logic to set json output property name to same as defined in class name. Normally we get id in JSON 
                 //when in class its defined as Id i-e camel case for all properties. So if we want the case to be same
                 //we use this logic
                 //.AddJsonOptions(o=>
