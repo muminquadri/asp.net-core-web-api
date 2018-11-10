97a535852d44f9812e0f17713f5a266a327163b0
Mumin Quadri <mumin.quadri25@gmail.com>
2018-11-11 02:35:08 +0530

    Adds entity classes for EF Core implementation

---
+++ CityInfo.Api/Entities/PointOfInterest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Entities
+{
+    public class PointOfInterest
+    {   [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+        //Name foreign key based on navigation property class name(City here) followed by Id
+        public int CityId { get; set; }
+     
+    }
+}
