f513ea07157ab295fd2d247742a330e50b2c9b50
Mumin Quadri <mumin.quadri25@gmail.com>
2018-11-28 08:22:25 +0530

    Playing with migrations

---
+++ CityInfo.Api/Migrations/20181128022949_TestMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CityInfo.Api.Migrations
+{
+    public partial class TestMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Cities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Cities",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
